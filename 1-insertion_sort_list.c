c97dd956f9752ff280a5bbef8118f12627c9d6ae
zelalem3 <zgetnet24@gmail.com>
2023-12-18 07:03:06 -0800

    Sorting algorithms

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,61 +1,102 @@
+
+Skip to main content
+Keyboard shortcuts
+Accessibility feedback
+Drive
+New
+Home
+My Drive
+Computers
+Shared with me
+Recent
+Starred
+Spam
+Trash
+Storage
+Get more storage
+
+
+Jan 21, 2023
+Jul 17, 2022
+Jul 17, 2022
+Jan 24, 2023
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+1-insertion_sort_list.c
+
+Details
+
+Activity
 #include "sort.h"
 
 /**
- * len_list - returns the length of a linked list
- * @h: pointer to the list
- *
- * Return: length of list
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
-int len_list(listint_t *h)
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	int len = 0;
-
-	while (h)
-	{
-		len++;
-		h = h->next;
-	}
-	return (len);
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
 /**
- * insertion_sort_list - sorts a linked list with the Insert Sort algorithm
- * @list: double pointer to the list to sort
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *curr = NULL, *one = NULL;
-	listint_t *two = NULL, *three = NULL, *four = NULL;
+	listint_t *iter, *insert, *tmp;
 
-	if (!list || !(*list) || len_list(*list) < 2)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	curr = *list;
-
-	while (curr)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		if (curr->prev && curr->n < curr->prev->n)
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			one = curr->prev->prev;
-			two = curr->prev;
-			three = curr;
-			four = curr->next;
-
-			two->next = four;
-			if (four)
-				four->prev = two;
-			three->next = two;
-			three->prev = one;
-			if (one)
-				one->next = three;
-			else
-				*list = three;
-			two->prev = three;
-			curr = *list;
-			print_list(*list);
-			continue;
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		else
-			curr = curr->next;
 	}
-}
+} 
