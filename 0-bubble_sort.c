3a77217f6a48c4f88c94910172040ed7ce8e53fe
zelalem3 <zgetnet24@gmail.com>
2023-12-18 06:08:22 -0800

    sorting slgorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,102 @@
+
+Skip to main content
+Keyboard shortcuts
+Accessibility feedback
+Drive
+New
+Home
+My Drive
+Computers
+Shared with me
+Recent
+Starred
+Spam
+Trash
+Storage
+Get more storage
+
+
+Jan 21, 2023
+Jul 17, 2022
+Jul 17, 2022
+Jan 24, 2023
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+Jul 17, 2022
+0-bubble_sort.c
+
+Details
+
+Activity
+Couldn't load details. Try again.
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * bubble_sort - sorts an array in ascending order
- * @array: array of ints to sort
- * @size: size of the array
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
-	int tmp;
+	size_t i, len = size;
+	bool bubbly = false;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	while (bubbly == false)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
+				bubbly = false;
 			}
 		}
+		len--;
 	}
 }
+
